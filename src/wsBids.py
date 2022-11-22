0cd7c798044a344242ebdf345f209ec647b40b3d
sepbos <sbostanb@ucsd.edu>
2022-11-21 21:38:49 -0800

    added a bit more comments

--- src/wsBids.py
+++ src/wsBids.py
@@ -90,3 +90,18 @@ def combine_tables(theTables):
     theTable.loc[theTable['Team'] == 'Rising Pune Supergiant',
                  'Team'] = 'Rising Pune Supergiants'
     theTable.to_csv('../data/IPL Player Auction 08-22.csv', index=False)
+
+
+'''
+these are the used parameters including urls for web scraping
+("https://en.wikipedia.org/wiki/List_of_2008_Indian_Premier_League_auctions_and_personnel_signings", 
+          2, [0, 1, 3, 4, 5], 2008, {"Name": "Player", "Auctioned Price(in US$ thousands)": "Amount"}, 81525)
+("https://en.wikipedia.org/wiki/List_of_2009_Indian_Premier_League_personnel_changes", 
+          7, [0, 4], 2009, {"Winning bid": "Amount"}, 81.53)
+("https://en.wikipedia.org/wiki/List_of_2010_Indian_Premier_League_personnel_changes", 
+          0, [3], 2010, {"Franchise": "Team", "Sold price (USD)": "Amount"}, 81.53)
+("https://en.wikipedia.org/wiki/List_of_2011_Indian_Premier_League_personnel_changes", 
+          2, [0, 4], 2011, {"Winning bid": "Amount"}, 81.53)
+("https://en.wikipedia.org/wiki/List_of_2012_Indian_Premier_League_personnel_changes", 
+          9, [0, 3], 2012, {"Winning bid": "Amount"}, 81.53)
+'''
